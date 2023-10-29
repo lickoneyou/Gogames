5589aca96e199d03450cc0762ad31b02480abe25
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-10-29 14:40:30 +0300

    completed about section

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Main.module.css";
 import Banner from "./Banner/Banner";
+import About from "./About/About";
 
 const Main = () => {
   return (
     <main>
       <Banner />
+      <About />
     </main>
   );
 };
