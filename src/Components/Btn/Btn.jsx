c429d80b263abfff7242f3117ca61e4848b192b4
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-07-01 11:46:46 +0300

    deleted all

--- src/Components/Btn/Btn.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./Btn.module.css";
-
-const Btn = ({ text = "Get Started Now", className, fn }) => {
-  return <button className={`${style.btn} ${className}`} onClick={fn}>{text}</button>;
-};
-
-export default Btn;
